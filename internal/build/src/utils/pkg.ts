13dcba0a22d5f5d19b39ae10e05261c5c7983d16
shuwen <1243889238@qq.com>
2024-01-31 09:49:38 +0800

    update: 解决打包过程中的报错

--- internal/build/src/utils/pkg.ts
+++ internal/build/src/utils/pkg.ts
@@ -7,7 +7,8 @@ export const pathRewriter = (module: Module) => {
 	const config = buildConfig[module];
 
 	return (id: string) => {
-		id = id.replaceAll(`lt-frame/`, `${config.bundle.path}/`);
+		id = id.replaceAll('@lt-frame/theme-chalk', 'lt-frame/theme-chalk');
+		id = id.replaceAll(`@lt-frame/`, `${config.bundle.path}/`);
 		return id;
 	};
 };
