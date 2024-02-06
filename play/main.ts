d8f5fac39b8dd313471c4c9255bc3949bb6c5505
shuwen <1243889238@qq.com>
2024-02-06 10:24:23 +0800

    update: main.ts

--- play/main.ts
+++ play/main.ts
@@ -1,12 +1,11 @@
 import { createApp } from 'vue';
-import { useNamespace } from '@lt-frame/hooks';
+// import { useNamespace } from '@lt-frame/hooks';
 import App from './App.vue';
+
 import '@lt-frame/theme-chalk/src/index.scss';
 import 'virtual:svg-icons-register';
 
-const ns = useNamespace('arrow');
-console.log(ns.b());
-console.log(ns.e('active'));
+// const ns = useNamespace('arrow');
 
 const app = createApp(App);
 
