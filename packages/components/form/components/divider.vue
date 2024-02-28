9ed5a7fcd0c00664df9a033111af88db2b41867f
shuwen <1243889238@qq.com>
2024-02-28 15:11:09 +0800

    update: 新增form组件，未完善

---
+++ packages/components/form/components/divider.vue
@@ -0,0 +1,11 @@
+<template>
+	<div :class="ns.b()">
+		<slot></slot>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { useNamespace } from '@lt-frame/hooks';
+
+const ns = useNamespace('form-divider ');
+</script>
