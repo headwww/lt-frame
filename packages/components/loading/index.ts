03d45ccf31f6bac133c254f902dd37c6cfd4f1bf
shuwen <1243889238@qq.com>
2024-03-15 16:20:50 +0800

    update: test

--- packages/components/loading/index.ts
+++ packages/components/loading/index.ts
@@ -1,7 +1,6 @@
-import { withInstall } from '@lt-frame/utils';
-import loading from './src/lt-loading.vue';
+import loading from './src/loading.vue';
 
-export const LTLoading = withInstall(loading);
+export const LTLoading = loading;
 
 export * from './src/loading';
 export * from './src/util';
