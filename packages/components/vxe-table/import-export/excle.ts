e70f211a74db384ab9c914f9649337193b99ab1d
shuwen <1243889238@qq.com>
2025-03-05 19:14:10 +0800

    update: 删除警告

--- packages/components/vxe-table/import-export/excle.ts
+++ packages/components/vxe-table/import-export/excle.ts
@@ -586,6 +586,7 @@ export const VXETablePluginExportXLSX = {
 	) {
 		// 检查版本
 		if (!/^(4)\./.test(vxetable.version) && !/v4/i.test((vxetable as any).v)) {
+			// eslint-disable-next-line no-console
 			console.error(
 				'[vxe-table-plugin-export-pdf 4.x] Version vxe-table 4.x is required'
 			);
